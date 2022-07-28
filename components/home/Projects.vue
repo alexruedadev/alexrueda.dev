4420c5cce6f08218bdccb89fc1e89f80545e26c1
alexruedadev <me@alexrueda.dev>
2022-07-28 23:48:14 +0200

    Devboard and projects updates

--- components/home/Projects.vue
+++ components/home/Projects.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="liner" class="relative max-w-7xl border-r border-l border-dashed py-6 mx-auto">
     
-    <div data-aos="zoom-in" class="select-none px-4 items-center justify-center sm:justify-start overflow-hidden flex pt-4 max-w-5xl">
+    <div data-aos="zoom-in" class="select-none px-4 items-center justify-center sm:justify-start overflow-hidden flex pt-4">
       <nav class="flex flex-wrap items-center justify-center flex-row space-x-2 sm:space-x-4" aria-label="Tabs">
         <button @click="currentTech = tech" :class="{ 'bg-gray-900 text-gray-300': tech === currentTech }" v-for="tech in techs" :key="tech"
                 class="flex text-gray-300 focus:outline-none focus:ring-transparent focus:ring-offset-transparent hover:text-hot-pink px-3 py-2 font-medium text-sm rounded-xl">
